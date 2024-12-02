99d2d16053b6685fd4d88c2327ee31292bfddb7b
ROSHAAN MAQSOOD <manjrajpoot892@gmail.com>
2024-12-03 01:52:37 +0500

    update css and service page and add service

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById("root")).render(
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="light"
+        theme="dark"
         toastClassName="toastBody"
       />
     </StrictMode>
