76f6ee7115e126ec51a5317c17cc657ff9146b4b
ROSHAAN MAQSOOD <manjrajpoot892@gmail.com>
2024-12-12 01:40:35 +0500

    add services and add toast when user logout

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,14 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../store/auth";
+import { toast } from "react-toastify";
 
 const Logout = () => {
   const { LogoutUser } = useAuth();
+  const didMount = useRef(false);
+
   useEffect(() => {
-    LogoutUser();
+    if (!didMount.current) {
+      didMount.current = true;
+      LogoutUser();
+      toast.success("Logout successful!");
+    }
   }, [LogoutUser]);
 
-  return <Navigate to="/login" />;
+  return (
+    <>
+      <Navigate to="/login" />
+    </>
+  );
 };
 
 export default Logout;
